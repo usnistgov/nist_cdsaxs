bf271694bca166bdc14cc09e6090a97c33ca9063
Wolf <caitlyn.wolf@nist.gov>
2025-03-20 17:35:29 -0400

    fixed namedtuple replace bug with params and sdd

--- src/cdsaxs_gui_legacy/processing.py
+++ src/cdsaxs_gui_legacy/processing.py
@@ -417,8 +417,7 @@ class ScatteringFile(object):
             self.sample_theta = info['sample_phi_deg']
             if params.normalize_exposure:
                 self.scaling_factor /= info['exposure_time_s']
-            params._replace(SDD_cm=info['sdd_cm'])
-            print(params)
+            params = params._replace(SDD_cm=info['sdd_cm'])
             self.dataqxzqy = DataQxzQy(fileformat, self.sample_theta, fullfilename, params, self.lambda_nm, self.scaling_factor)
         elif fileformat == 'bin':
             self.sample_theta = info['Sample Theta']
