1f79b6d04cb7f8f2229e103ffed276394fc7abf5
Wolf <caitlyn.wolf@nist.gov>
2025-03-21 15:49:11 -0400

    added a Dataset class

--- src/cdsaxs/data.py
+++ src/cdsaxs/data.py
@@ -1,13 +1,25 @@
 """
-This module contains the class for 2D image data and relevant metadata:
-DataQxzQy. An instance of this class should be built for every
-scattering image.
+This module contains two classes:
+
+DataQxzQy : Class for storing a single scattering image, relevant
+    scattering metadata, and any user-defined parameters.
+Dataset : Class for managing a series of DataQxzQy objects. A new
+    Dataset instance is created upon each loading of data by the user
+    in the GUI.
 """
 
 from __future__ import annotations
 
+import os
+
 import numpy as np
 from numpy.typing import NDArray
+
+METADATA_KEYWORDS = [
+        'sample_phi_deg', 'energy_ev', 'wavelength_nm', 'scaling_factor',
+        'detector_theta', 'detector_x', 'exposure_time_s', 'sdd_cm',
+        'sample_chi_deg', 'div_photodiode', 'center_px',
+    ]
 
 
 class DataQxzQy():
@@ -41,24 +53,18 @@ class DataQxzQy():
     TODO : add brief definitions to these keywords
     Metadata Keywords
     -----------------
-    sample_phi_deg
-    energy_ev
-    wavelength_nm
+    sample_phi_deg : sample rotation angle about positive y axis
+    energy_ev : source energy, eV
+    wavelength_nm : source wavelength, nanometers
     scaling_factor : data scaling factor, defaults to 1
     detector_theta
     detector_x
     exposure_time_s : count time in seconds
     sdd_cm : sample to detector distance in cm
-    sample_chi_deg : rotation about the z-axis (beam directory)
+    sample_chi_deg : rotation about the z-axis (beam direction)
     div_photodiode
     center_px : [xz, y] beam center pixel location in xz and y
     """
-
-    accepted_keywords = [
-        'sample_phi_deg', 'energy_ev', 'wavelength_nm', 'scaling_factor',
-        'detector_theta', 'detector_x', 'exposure_time_s', 'sdd_cm',
-        'sample_chi_deg', 'div_photodiode', 'center_px',
-    ]
 
     _current_rotation = 0
 
@@ -77,14 +83,14 @@ class DataQxzQy():
         self.qys = qys
 
         unaccepted_keywords = [
-            x for x in metadata.keys() if x not in self.accepted_keywords
+            x for x in metadata.keys() if x not in METADATA_KEYWORDS
         ]
         if len(unaccepted_keywords) > 0:
             raise ValueError(
                 "The following metadata keywords are not accepted:\n" +
                 f"{unaccepted_keywords}\n" +
-                "The following are accepted metadata keywords:\n"+
-                f"{self.accepted_keywords}"
+                "The following are accepted metadata keywords:\n" +
+                f"{METADATA_KEYWORDS}"
             )
 
         # TODO : implement checks for missing critical metadata
@@ -138,17 +144,20 @@ class DataQxzQy():
         if k == 1:
             self.qxzs = qys
             self.qys = -1*np.flip(qxzs)
-            self.metadata['center_px'] = [len(qxzs)-center_px[1]-1, center_px[0]]
+            self.metadata['center_px'] = [
+                len(qxzs)-center_px[1]-1, center_px[0]]
 
         if k == 2:
             self.qxzs = -1*np.flip(qxzs)
             self.qys = -1*np.flip(qys)
-            self.metadata['center_px'] = [len(qys)-center_px[0]-1, len(qxzs)-center_px[1]-1]
+            self.metadata['center_px'] = [
+                len(qys)-center_px[0]-1, len(qxzs)-center_px[1]-1]
 
         if k == 3:
             self.qxzs = -1*np.flip(qys)
             self.qys = qxzs
-            self.metadata['center_px'] = [center_px[1], len(qys)-center_px[0]-1]
+            self.metadata['center_px'] = [
+                center_px[1], len(qys)-center_px[0]-1]
 
         self._current_rotation += k
 
@@ -160,3 +169,60 @@ class DataQxzQy():
         """
         if self._current_rotation > 0:
             self.rotate_image(-90*self._current_rotation)
+
+    def update_beamcenter(self, qy, qxz):
+        """Udpdate the beam center indices."""
+        self.metadata['center_px'] = [qy, qxz]
+        self.recalculate_q()
+
+    def recalculate_q(self):
+        """
+        Recalculate scattering vectors qys and qxzs.
+
+        This should be performed after any changes to the beam center
+        position or detector position.
+        """
+        # TODO: implement when working on diffraction.py
+        pass
+
+
+class Dataset():
+    """
+    This class manages at least one instance of DataQxzQy as a dataset.
+
+    Attributes
+    ----------
+    data_folder : absolute filepath to data directory
+    name : default is data_folder but can be user-specified
+    datas : dictionary of filename keys leading to DataQxzQy objects,
+        one for each scattering file
+    """
+
+    def __init__(
+        self,
+        data_folder: str,
+        name: str = None
+    ):
+        self.data_folder = os.path.abspath(data_folder)
+
+        if name is not None:
+            self.name = name
+        else:
+            self.name = self.data_folder
+
+        self.data = {}
+
+    def add_data(self, filename: str, data: DataQxzQy):
+        """Add a single DataQxzQy instance to the dataset."""
+        if filename in self.datas.keys():
+            raise ValueError(
+                f"Data for {filename} is already part of this dataset")
+        self.datas[filename] = data
+
+    def remove_data(self, filename):
+        """Removes a single DataQxzQy instance from the dataset."""
+        try:
+            del self.datas[filename]
+        except KeyError:
+            print(f"Could not delete {filename} data as it was not part of the"
+                  "dataset.")
