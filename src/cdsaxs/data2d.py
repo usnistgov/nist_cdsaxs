4dc69a62fc7076e10e4cea175c47a99b9ada28de
Wolf <caitlyn.wolf@nist.gov>
2025-08-15 15:46:07 -0400

    fixed bug in normalization

--- src/cdsaxs/data2d.py
+++ src/cdsaxs/data2d.py
@@ -285,7 +285,7 @@ class Data2D():
                     "Size of the provided array does not"
                     "match the size of the image data.")
 
-        self.scale_data(value)
+        self.scale_data(value_r)
         self._data_transformations.append(("normalize", value))
 
     def subtract_from_data(self, value):
