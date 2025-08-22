8fa53d76b9698341b28ccfd845e7a41eee62e09a
Wolf <caitlyn.wolf@nist.gov>
2025-08-22 14:45:26 -0400

    added tests for metadata.py

---
+++ tests/test_cdsaxs/test_metadata.py
@@ -0,0 +1,69 @@
+import unittest
+
+from cdsaxs.metadata import correct_metadata_dtype, check_metadata
+from cdsaxs.metadata import METADATA_KEYWORDS, FLOATS, INTEGERS, STRINGS
+
+
+class TestCorrectMetadataDtype(unittest.TestCase):
+
+    def test_floats(self):
+        string_input = 20.2
+        for key in FLOATS:
+            self.assertAlmostEqual(
+                correct_metadata_dtype(key, string_input),
+                20.2
+            )
+
+    def test_integers(self):
+
+        float_input = 20.2
+        for key in INTEGERS:
+            self.assertEqual(
+                correct_metadata_dtype(key, float_input),
+                20
+            )
+
+    def test_strings(self):
+        float_input = 20.2
+        for key in STRINGS:
+            self.assertEqual(
+                correct_metadata_dtype(key, float_input),
+                "20.2"
+            )
+
+    def test_no_dtype(self):
+
+        typed_keywords = FLOATS + STRINGS + INTEGERS
+        value = (1, 2, 3)
+        for key in METADATA_KEYWORDS:
+            if key not in typed_keywords:
+                self.assertTupleEqual(
+                    correct_metadata_dtype(key, value),
+                    value
+                )
+
+
+class TestCheckMetadata(unittest.TestCase):
+
+    def test_operational(self):
+
+        self.assertTrue(
+            check_metadata({
+                'sample_phi_deg': -40,
+                'energy_ev': 16100,
+            })
+        )
+
+    def test_unaccepted_keyswords(self):
+
+        with self.assertRaises(ValueError):
+            check_metadata({'not a keyword': 2})
+
+    def test_overspecified_source(self):
+        """Check energy OR wavelength is provided, not both."""
+
+        with self.assertRaises(ValueError):
+            check_metadata({
+                'energy_ev': 16100,
+                'wavelength_nm': 1.2
+            })
