8d38009dd2f344c548b2598bc7cac433fe846de6
Wolf <caitlyn.wolf@nist.gov>
2025-09-17 15:16:27 -0400

    fixing nan handling in the reduction slices

--- src/cdsaxs/reduction.py
+++ src/cdsaxs/reduction.py
@@ -439,9 +439,8 @@ def slice_reduced_dataset(
 
         for data in dataset.reduced_datasets[reduced_index].values():
             selection = np.where((data.qsx >= q_range[0]) &
-                                 (data.qsx <= q_range[1]) &
-                                 (~np.isnan(data.Iq)))[0]
-            if len(selection) > 0:
+                                 (data.qsx <= q_range[1]))[0]
+            if len(selection) > 0 and not np.isnan(data.Iq[selection]).any():
                 qsz.append(np.nanmean(data.qsz[selection]))
                 Iq.append(np.nanmean(data.Iq[selection]))
 
