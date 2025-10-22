fixing default masking for both 1d and 2d data

src/cdsaxs/data/data1d.py
@@ -13,6 +13,7 @@ from __future__ import annotations
 import numpy as np
 from numpy.typing import NDArray
 
+from cdsaxs.tools import default_mask
 from cdsaxs.data.metadata import ACCEPTED_Q_AXES
 from cdsaxs.plotting import plotting
 
@@ -93,14 +94,10 @@ class Data1D():
         # check q axes with length of Iq
         self.Iq = np.array(Iq).reshape(-1).astype(float)
         self._raw_Iq = np.copy(Iq)
-        self.mask = np.isnan(self.Iq)
+
+        self.mask = default_mask(self.Iq)
         if mask is not None:
-            if len(mask) != len(self.Iq):
-                raise ValueError(
-                    "The mask and Iq should be of same length."
-                )
-            else:
-                self.mask += mask
+            self.mask_points(mask)
 
         q = np.array(q).reshape(-1).astype(float)
         if len(q) != len(self.Iq):
@@ -342,6 +339,11 @@ class Data1D():
             all data operations. This will NOT unmask any previously
             masked points.
         """
+        mask = mask.reshape(-1)
+        if len(mask) != len(self.Iq):
+            raise ValueError(
+                "Mask does not match shape of Iq."
+            )
         self.mask += mask
 
     def overwrite_mask(self, mask):

src/cdsaxs/data/data_image.py
@@ -56,7 +56,7 @@ class DataImage():
         self._raw_image = np.copy(self.image)
         self.mask = default_mask(self.image)
         if mask is not None:
-            self.mask += mask  # apply user-provided mask
+            self.mask_points(mask)
         self._data_transformations = []
 
     @property
@@ -78,6 +78,10 @@ class DataImage():
             all data operations. This will NOT unmask any previously
             masked points.
         """
+        if mask.shape != self.image.shape:
+            raise ValueError(
+                "Mask does not match shape of image."
+            )
         self.mask += mask
 
     def _overwrite_mask(self, mask):

src/cdsaxs/data/dataset.py
@@ -802,7 +802,6 @@ class ReducedSlices():
     def export_reduced_slices(
             self,
             filepath,
-            index=None,
             filter_by_q={},
             q_axis='qsz',
             integrated_axis='qsx',
@@ -842,7 +841,7 @@ class ReducedSlices():
             Iq = getattr(r_slice, '_masked_Iq')
             q_int = getattr(r_slice, integrated_axis)
 
-            #sort by q
+            # sort by q
             sorted_indexes = np.argsort(q)
             q = q[sorted_indexes]
             Iq = Iq[sorted_indexes]