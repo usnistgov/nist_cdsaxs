2a39689d4c70a643f18a4d2c5c2884262be073cd
Wolf <caitlyn.wolf@nist.gov>
2026-04-07 15:54:07 -0400

    updated ALS loader tests

--- tests/test_cdsaxs/test_loaders/test_filetypes.py
+++ tests/test_cdsaxs/test_loaders/test_filetypes.py
@@ -67,3 +67,68 @@ class TestReadNistBin(unittest.TestCase):
             metadata["pixel_size_um"],
             172
         )
+
+
+class TestReadALS(unittest.TestCase):
+
+    def setUp(self):
+        image_file = "../../data/test_loaders/jk_srm_i10_300ev_align87448-00038.fits"
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.filepath = os.path.abspath(
+            os.path.join(current_dir, image_file))
+        self.return_read = filetypes.read_als_11_0_1_2(self.filepath)
+
+    def test_image(self):
+        # TODO: implement this test
+        pass
+
+    def test_metadata(self):
+        metadata_return = self.return_read[-1]
+
+        self.assertAlmostEqual(
+            metadata_return["energy_ev"],
+            300.011826472274)
+        self.assertAlmostEqual(
+            metadata_return["sample_phi_deg"],
+            -90 + 90  # adjust for different coordinate systems
+        )
+        self.assertAlmostEqual(
+            metadata_return["I0"],
+            0.0579485378986026
+        )
+        self.assertAlmostEqual(
+            metadata_return["exposure_time_s"],
+            0.00999999977648258
+        )
+        self.assertAlmostEqual(
+            metadata_return["beam_current"],
+            500.108294433594
+        )
+        self.assertAlmostEqual(
+            metadata_return["detector_phi_deg"],
+            17
+        )
+        self.assertAlmostEqual(
+            metadata_return["detector_y_mm"],
+            98
+        )
+        self.assertAlmostEqual(
+            metadata_return["CCD Y"],
+            20
+        )
+        self.assertAlmostEqual(
+            metadata_return["epu_polarization"],
+            100
+        )
+        self.assertAlmostEqual(
+            metadata_return["beam_stop_position"],
+            8
+        )
+        self.assertAlmostEqual(
+            metadata_return["pixel_size_um"],
+            27
+        )
+
+    def test_pixel_size(self):
+        # TODO: implement a more robust test for pixel size if/else
+        pass
