8156723bfa519d7090e3082fcea9b154f4fd99d5
MatthewAWade <matthew.wade@nist.gov>
2026-05-28 16:59:08 -0400

    removed array in saved sample_phi_deg metadata when pulled from the hdf file format

--- src/cdsaxs/loaders/filetypes.py
+++ src/cdsaxs/loaders/filetypes.py
@@ -139,7 +139,7 @@ def read_smi_h5(filepath):
         temp_metadata = dict(metadata)
         metadata_index = np.where(seq_num == i+1)[0]
         temp_metadata['bpm'] = metadata['bpm'][metadata_index]
-        temp_metadata['sample_phi_deg'] = np.round(-1*metadata['sample_phi_deg'][metadata_index],2)
+        temp_metadata['sample_phi_deg'] = np.round(-1*metadata['sample_phi_deg'][metadata_index],2)[0]
         images.append(
             (image_stack[i].astype(np.float64), filepath, temp_metadata)
         )
