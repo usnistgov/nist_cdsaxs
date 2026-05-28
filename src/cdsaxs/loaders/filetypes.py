a84b00133f2c3481b8839d4e0036a87d0171a1bf
Wolf <caitlyn.wolf@nist.gov>
2026-05-28 16:29:30 -0400

    fixed indexing issue; base 0 in python but base 1 in the h5 file

--- src/cdsaxs/loaders/filetypes.py
+++ src/cdsaxs/loaders/filetypes.py
@@ -137,8 +137,8 @@ def read_smi_h5(filepath):
     images = []
     for i in range(num_images):
         temp_metadata = dict(metadata)
-        temp_metadata['bpm'] = metadata['bpm'][int(seq_num[i])]
-        temp_metadata['sample_phi_deg'] = np.round(-1*metadata['sample_phi_deg'][int(seq_num[i])],2)
+        temp_metadata['bpm'] = metadata['bpm'][int(seq_num[i])-1]
+        temp_metadata['sample_phi_deg'] = np.round(-1*metadata['sample_phi_deg'][int(seq_num[i])-1],2)
         images.append(
             (image_stack[i].astype(np.float64), filepath, temp_metadata)
         )
