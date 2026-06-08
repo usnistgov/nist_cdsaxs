23a795d6afdac3945d6c8c70aacdde116d4407cc
MatthewAWade <matthew.wade@nist.gov>
2026-06-08 16:26:56 -0400

    Fix to type error - only 0 dimensional arrays can be converted to python scalars. Identified with @lgackey.

--- src/cdsaxs/tools.py
+++ src/cdsaxs/tools.py
@@ -112,7 +112,7 @@ def line_fit(x, y, force_intercept=None):
             fit_intercept=False if force_intercept is not None else True)
         model.fit(x, y)
         slope = float(model.coef_[0][0])
-        intercept = float(model.intercept_)
+        intercept = float(model.intercept_[0])
         if force_intercept is not None:
             intercept = force_intercept[1] - slope * force_intercept[0]
         angle = np.rad2deg(np.arctan(slope))
