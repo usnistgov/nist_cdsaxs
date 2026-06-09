1538b930044e2b2b0aecba86910d0b51262cb782
MatthewAWade <matthew.wade@nist.gov>
2026-06-09 10:28:51 -0400

    Quick bandaid solution for the tighter matrix dimensionality restrictions. Looking for alternatives.

--- src/cdsaxs/tools.py
+++ src/cdsaxs/tools.py
@@ -112,7 +112,10 @@ def line_fit(x, y, force_intercept=None):
             fit_intercept=False if force_intercept is not None else True)
         model.fit(x, y)
         slope = float(model.coef_[0][0])
-        intercept = float(model.intercept_[0])
+        try:
+            intercept = float(model.intercept_[0])
+        except:
+            intercept=float(model.intercept_)
         if force_intercept is not None:
             intercept = force_intercept[1] - slope * force_intercept[0]
         angle = np.rad2deg(np.arctan(slope))
