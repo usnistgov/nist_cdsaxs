updated smi h5 loader to include epoch time in user_params and also scale sample_phi_deg by -1 if the measurement occured before July 8 when the rotation direction was corrected at the beamline to follow right hand rule

src/cdsaxs/loaders/filetypes.py
@@ -1,3 +1,5 @@
+from datetime import datetime, date
+from zoneinfo import ZoneInfo
 import os
 
 import h5py
@@ -92,6 +94,7 @@ def read_nist_bin(filepath):
 
     return image, filepath, metadata
 
+
 def read_smi_h5(filepath):
     """
     Load an image and metadata from an H5 file from the SMI beamline
@@ -114,11 +117,23 @@ def read_smi_h5(filepath):
 
     # load the entire scan
     scan = h5py.File(filepath, 'r')
-    
+
     # extract image stack and number of images in the scan
     image_stack = scan['raw_images']['pil2M_image'][:]
     num_images = image_stack.shape[0]
-    
+
+    # scale phi by +1 or -1 depending on when the h5 file was exported
+    # Eliot fixed the rotation direction to follow right-hand rule on
+    # July 8, 2026; before this date, scale phi by -1
+    phi_scale = 1.0
+    timestamp = scan['baseline']['time'][0]
+    timestamp = datetime.fromtimestamp(
+        timestamp, tz=ZoneInfo("America/New_York"))
+    check_day = timestamp.date()
+    phi_fix_day = date(2026, 7, 8)
+    if check_day < phi_fix_day:
+        phi_scale = -1.0
+
     metadata = {}
     metadata['energy_ev'] = scan['baseline']['energy_energy'][0]
     metadata['sdd_cm'] = scan['baseline']['pil2M_motor_z'][0]/10
@@ -126,15 +141,17 @@ def read_smi_h5(filepath):
     metadata['pixel_size_um'] = 172  # pilatus2m
     metadata['bpm'] = scan['primary']['xbpm3_sumX'][:]
     metadata['sample_phi_deg'] = scan['primary']['stage_phi'][:]
+    metadata['epoch_time'] = scan['primary']['time']
 
-    seq_num = scan['primary']['seq_num'][:]  
+    seq_num = scan['primary']['seq_num'][:]
 
     images = []
     for i in range(num_images):
         temp_metadata = dict(metadata)
         metadata_index = np.where(seq_num == i+1)[0]
         temp_metadata['bpm'] = metadata['bpm'][metadata_index][0]
-        temp_metadata['sample_phi_deg'] = np.round(-1*metadata['sample_phi_deg'][metadata_index],2)[0]
+        temp_metadata['sample_phi_deg'] = np.round(phi_scale*metadata['sample_phi_deg'][metadata_index], 2)[0]
+        temp_metadata['epoch_time'] = metadata['epoch_time'][metadata_index][0]
         images.append(
             (image_stack[i].astype(np.float64), filepath, temp_metadata)
         )