38c3255a21e2cbb42cd0b178fab940f766a8efc5
Wolf <caitlyn.wolf@nist.gov>
2026-07-24 14:54:53 -0400

    removed legacy code from the export slice function

--- src/cdsaxs/data/dataset.py
+++ src/cdsaxs/data/dataset.py
@@ -945,18 +945,14 @@ class ReducedSlices():
             self,
             filepath,
             filter_by_q={},
-            q_axis='qsz',
-            integrated_axis='qsx',
-            offset_axis='qsy',
             export_qr=False,
-            decimals=5):
+            decimals=7):
         """
         Outputs the slected reduced slices set currently stored in the
-        dataset to a csv file.
+        dataset to a csv file. All qsx, qsy, qsz, and masked Iq will be
+        exported. The user can also choose to export qsr.
+        TODO: allow user to select the specific axes for export
 
-        NOTE: currently only a q_slice_axis of 'qsx' is accepted or
-        formatted appropriately in the output file.
-        TODO: generalize this in the future.
 
         Parameters
         ----------
@@ -965,16 +961,6 @@ class ReducedSlices():
         filter_by_q : dict
             Dictionary of q component and range (min, max) to filter
             the slices by.
-        TODO: Add more options for exporting axis. Currently hardcoded.
-        q_axis : str
-            Currently only 'qsz' is accepted.
-            Default is 'qsz'.
-        integrated_axis : str
-            Currently only 'qsx' is accepted.
-            Default is 'qsx'.
-        offset_axis : str
-            Currently only 'qsy' is accepted.
-            Default is 'qsy'.
         export_qr : bool
             If set to True, qsr will also be exported in the file.
         decimals : int
@@ -999,48 +985,45 @@ class ReducedSlices():
                     keep.append(False)
             filtered_slices = [x for x, k in zip(filtered_slices, keep) if k]
 
+        # figure out the max length to pad ends of slices with empty values
+        # for the csv export (needs a table, not arrays of different lengths)
         length = 0
         for r_slice in filtered_slices:
-            length = np.max((length, len(getattr(r_slice, q_axis))))
+            length = np.max((length, len(getattr(r_slice, 'qsz'))))
 
         datas = []
 
         for r_slice in filtered_slices:
-            q = getattr(r_slice, q_axis)
+            qz = getattr(r_slice, 'qsz')
             Iq = getattr(r_slice, '_masked_Iq')
-            q_int = getattr(r_slice, integrated_axis)
-            q_offset = getattr(r_slice, offset_axis)
-
-            # Data1D stores scalar q metadata as length-1 arrays; flatten so
-            # exported headers contain plain values like 0.4 instead of [0.4].
-            if np.size(q_int) == 1:
-                q_int = np.asarray(q_int).reshape(-1)[0]
+            qx = getattr(r_slice, 'qsx')
+            qy = getattr(r_slice, 'qsy')
 
             # sort by q
-            sorted_indexes = np.argsort(q)
-            q = q[sorted_indexes]
-            q_offset = q_offset[sorted_indexes]
+            sorted_indexes = np.argsort(qz)
+            qz = qz[sorted_indexes]
             Iq = Iq[sorted_indexes]
+            qy = qy[sorted_indexes]
+            qx = np.tile(np.asarray(qx), np.size(qz))  # qx is single value
 
+            # only export the data that is non NAN and above 0 as the
+            # analysis code does not accept intensity values of 0
             select = (~np.isnan(Iq)) & (Iq > 0)
-            num_points = len(q[select])
 
-            # Create columns for qx,qy,qr(if header axis == qsr is specified)
-
-            new_qx = np.hstack(                 #integration axis
+            new_qx = np.hstack(
                 ([r'$q_x (\AA^{-1})$'],
-                    [str(np.round(q_int, decimals))]*num_points,
-                    [""]*(length-len(q[select])))
+                    np.round(qx, decimals).astype(str)[select],
+                    [""]*(length-len(qx[select])))
             )
-            new_qy = np.hstack(                 #offset axis
+            new_qy = np.hstack(
                 ([r'$q_y (\AA^{-1})$'],
-                    np.round(q_offset, decimals).astype(str)[select],
-                    [""]*(length-len(q_offset[select])))
+                    np.round(qy, decimals).astype(str)[select],
+                    [""]*(length-len(qy[select])))
             )
-            new_qz = np.hstack(                 #q axis that data are plotted along
+            new_qz = np.hstack(
                 ([r'$q_z (\AA^{-1})$'],
-                    np.round(q, decimals=decimals).astype(str)[select],
-                    [""]*(length-len(q[select])))
+                    np.round(qz, decimals=decimals).astype(str)[select],
+                    [""]*(length-len(qz[select])))
             )
 
             datas.append(new_qx)
